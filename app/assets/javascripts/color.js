6d6533cecb178b8508893596cdecdf6a6679c764
macken-hem <ken.kgaf@gmail.com>
2019-11-12 23:10:01 +0900

    up

--- app/assets/javascripts/color.js
+++ app/assets/javascripts/color.js
@@ -1,7 +1,7 @@
 $(function(){
   $(".left_hover_content").hover(
     $(function(){
-      $(".left_hover_text").fadeIn();
+      $(".left_hover_text").fadeIn(1000);
     },
     function(){
       $(".left_hover_text").fadeOut();
