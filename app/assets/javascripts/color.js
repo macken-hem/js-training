4b22b07c5990956d661aea0232e799b88f983ce8
macken-hem <ken.kgaf@gmail.com>
2019-11-17 20:54:39 +0900

    Update color.js

--- app/assets/javascripts/color.js
+++ app/assets/javascripts/color.js
@@ -1,1 +1,11 @@
-window.alert('こんにちは');
+$(function(){
+
+  $('.left_hover_content').hover(
+    function(){
+      $('.left_hover_text').fadeIn();
+    },
+    function(){
+      $('.left_hover_text').fadeOut();
+    }
+  )
+});
