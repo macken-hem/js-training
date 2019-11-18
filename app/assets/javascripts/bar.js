adc96091ade91c8ded3bf3dce6cd51a8b755a0db
macken-hem <ken.kgaf@gmail.com>
2019-11-18 13:54:12 +0900

    up

---
+++ app/assets/javascripts/bar.js
@@ -0,0 +1,9 @@
+$(function(){
+  $('.fas.fa-bars').on("click", function(){
+    $('.top_side').fadeIn();
+  });
+  $('.fas.fa-times').on("click", function(){
+    $('.top_side').fadeOut();
+  });
+
+});
